15334edac2efee248ab3e9436f3ebce8ea7b5786
MuhammedRashik <razikrashik8@gmail.com>
2023-11-06 07:52:16 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { EffectComposer, Noise } from "@react-three/postprocessing";
 import { Experience } from "./components/Experience";
+import { Overlay } from "./components/Overlay";
+import { usePlay } from "./contexts/Play";
 
 function App() {
+  const { play, end } = usePlay();
+
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls
+          pages={play && !end ? 20 : 0}
+          damping={0.5}
+          style={{
+            top: "10px",
+            left: "0px",
+            bottom: "10px",
+            right: "10px",
+            width: "auto",
+            height: "auto",
+            animation: "fadeIn 2.4s ease-in-out 1.2s forwards",
+            opacity: 0,
+          }}
+        >
+          <Experience />
+        </ScrollControls>
+        <EffectComposer>
+          <Noise opacity={0.01} />
+        </EffectComposer>
+      </Canvas>
+      <Overlay />
+    </>
   );
 }
 
